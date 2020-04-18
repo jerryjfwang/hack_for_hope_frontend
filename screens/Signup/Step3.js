0d98b0041a0834ca4a57d1c111efe44d2f26571a
Jerry Wang <jerry@jerrys-mbp.lan>
2020-04-17 18:30:02 -0700

    Adding border active styles for signup buttons

--- screens/Signup/Step3.js
+++ screens/Signup/Step3.js
@@ -45,21 +45,20 @@ const TimeOptions = ({ preferences, setPreferences }) => (
 export default Step3;
 
 const OptionButton = styled(TouchableOpacity)`
-      border-radius: 10px;
-      background: ${({ active }) => (active ? "#20BF6B" : "#FFFFFF")};
-      border: 1px solid rgba(170, 170, 170, 0.8);
-      width: 100%
-      display: flex;
-      justify-content: center;
-      align-items:center;
-      padding: 14px;
+    border-radius: 10px;
+    background: ${({ active }) => (active ? "#20BF6B" : "#FFFFFF")};
+    border: ${({ active }) =>
+      active ? "0px" : "1px solid rgba(170, 170, 170, 0.8"};
+    width: 100%
+    display: flex;
+    justify-content: center;
+    align-items:center;
+    padding: 14px;
     margin-top: 16px;
 `;
 const Option = styled(Text)`
   color: ${({ active }) => (active ? "#FFFFFF" : "#000")};
-
   font-family: Rubik-Bold;
-
   text-align: center;
   font-size: 18px;
 `;
